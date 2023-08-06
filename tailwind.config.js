234f94b5c57f82dc5a9ebb02b9b1366f7d222d8a
K3nK0 <uncommonmen@hotmail.fr>
2023-08-06 23:44:09 +0200

    img + title

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {},
+    colors: {
+      softBlue: 'hsl(215, 51%, 70%)',
+      cyan: 'hsl(178, 100%, 50%, 50%)',
+      cyanTxt: 'hsl(178, 100%, 50%)',
+      mainDarkBlue: 'hsl(217, 54%, 11%)',
+      cardDarkBlue: 'hsl(216, 50%, 16%)',
+      lineDarkBlue: 'hsl(215, 32%, 27%)',
+      white: 'hsl(0, 0%, 100%)',
+    },
   },
   plugins: [],
 }
